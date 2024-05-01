c5416838cc948731fbc1fe789e099c80ac6765dd
vedant-anand-0 <vedantanandva28@gmail.com>
2024-05-01 19:17:34 +0530

    fix: minor

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,3 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: "GChjw0nAGTDFpXj5lRVANCWp",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
+
 });
